8b6280134525353981dac98b5085762cabb87a93
hmarianno <hmarianno@gmail.com>
2021-05-20 12:27:46 -0300

    Obtendo um usuário por id

--- src/main/java/com/hmarianno/course/services/UserService.java
+++ src/main/java/com/hmarianno/course/services/UserService.java
@@ -1,12 +1,14 @@
 package com.hmarianno.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hmarianno.course.domain.User;
 import com.hmarianno.course.repository.UserRepository;
+import com.hmarianno.course.services.exception.ObjectNotFoundException;
 
 // serviço é a classe que conversa com a camada de acesso a 
 // dados (interface repositório) - ver no início do material 
@@ -21,5 +23,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll(); // retorna uma lista de usuários
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
